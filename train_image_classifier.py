fff8f63bdf4be355ad1a1c22c8f5c2ded5ef9f1c
jy <johniao@163.com>
2018-10-22 10:15:17 +0800

    w

--- train_image_classifier.py
+++ train_image_classifier.py
@@ -405,7 +405,7 @@ def main(_):
 
     # Create global_step
     with tf.device(deploy_config.variables_device()):
-      global_step = slim.create_global_step()
+      global_step = tf.train.create_global_step()
 
     ######################
     # Select the dataset #
