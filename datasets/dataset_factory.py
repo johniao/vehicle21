811a8472500a062047ad556e3fea1f14e4af3859
jy <johniao@163.com>
2018-10-22 14:40:22 +0800

    w

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -22,10 +22,12 @@ from datasets import cifar10
 from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
+from datasets import vehicle
 
 datasets_map = {
     'cifar10': cifar10,
     'flowers': flowers,
+    'quiz': vehicle,
     'imagenet': imagenet,
     'mnist': mnist,
 }
